d4b22bf81726d8fa067f8c97bde09fcb9a98481d
swediRoben <farajampenda43@gmail.com>
2026-02-22 17:55:37 +0200

    journal de la tresorerie

--- src/main/java/com/app/budget/tresorerie/controller/JournalTresorerieController.java
+++ src/main/java/com/app/budget/tresorerie/controller/JournalTresorerieController.java
@@ -1,5 +1,74 @@
 package com.app.budget.tresorerie.controller;
 
+import java.time.OffsetDateTime;
+import java.util.List;
+
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.app.budget.tresorerie.dto.JournalTresorerieDto;
+import com.app.budget.tresorerie.service.JournalTresorerieService;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody; 
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/journal-tresorerie")
+@RequiredArgsConstructor
+@Validated
 public class JournalTresorerieController {
-    
+
+    private final JournalTresorerieService service;
+
+    // ================= CREATE =================
+    @PostMapping
+    public JournalTresorerieDto create(@RequestBody JournalTresorerieDto dto) {
+        return service.create(dto);
+    }
+
+    // ================= UPDATE =================
+    @PutMapping("/{id}")
+    public JournalTresorerieDto update(
+            @PathVariable Long id,
+              @RequestBody JournalTresorerieDto dto) {
+        return service.update(id, dto);
+    }
+
+    // ================= GET BY ID =================
+    @GetMapping("/{id}")
+    public JournalTresorerieDto getById(@PathVariable Long id) {
+        return service.getById(id);
+    }
+
+    // ================= DELETE =================
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        service.delete(id);
+    }
+
+    // ================= FIND ALL =================
+    @GetMapping
+    public List<JournalTresorerieDto> findAll(
+            @RequestParam(required = false) Long exerciceId,
+            @RequestParam(required = false) Long banqueId,
+            @RequestParam(required = false) Long compteBancaireId,
+            @RequestParam(required = false) String numero,
+            @RequestParam(required = false) OffsetDateTime debut,
+            @RequestParam(required = false) OffsetDateTime fin) {
+
+        return service.findAll(
+                exerciceId,
+                banqueId,
+                compteBancaireId,
+                numero,
+                debut,
+                fin);
+    }
 }
--- src/main/java/com/app/budget/tresorerie/service/JournalTresorerieService.java
+++ src/main/java/com/app/budget/tresorerie/service/JournalTresorerieService.java
@@ -1,8 +1,176 @@
 package com.app.budget.tresorerie.service;
 
+import java.time.OffsetDateTime;
+import java.util.List;
+
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
+import com.app.budget.repos.ClasseRepository;
+import com.app.budget.repos.DeviseRepository;
+import com.app.budget.repos.LiquidationRepository;
+import com.app.budget.repos.PlanActiviteRepository;
+import com.app.budget.repos.PlanComptableRepository;
+import com.app.budget.repos.SourceFinacementRepository;
+import com.app.budget.tresorerie.dto.JournalTresorerieDto;
+import com.app.budget.tresorerie.entity.JournalTresorerie;
+import com.app.budget.tresorerie.repository.BanqueRepository;
+import com.app.budget.tresorerie.repository.CompteBancaireRepository;
+import com.app.budget.tresorerie.repository.JournalTresorerieRepository;
+import com.app.budget.tresorerie.repository.JournalTresorerieSpecification;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+ 
 @Service
+@RequiredArgsConstructor
+@Transactional
 public class JournalTresorerieService {
-    
+
+    private final JournalTresorerieRepository repository;
+    private final BanqueRepository banqueRepository;
+    private final PlanComptableRepository planComptableRepository;
+    private final ClasseRepository classeRepository;
+    private final DeviseRepository deviseRepository;
+    private final CompteBancaireRepository compteBancaireRepository;
+    private final LiquidationRepository liquidationRepository;
+    private final PlanActiviteRepository planActiviteRepository;
+    private final SourceFinacementRepository sourceFinacementRepository;
+
+    // ================= CREATE =================
+    public JournalTresorerieDto create(JournalTresorerieDto dto) {
+        JournalTresorerie entity = new JournalTresorerie();
+         validateReferences(dto);
+        mapToEntity(dto, entity);
+        return toDto(repository.save(entity));
+    }
+
+    // ================= UPDATE =================
+    public JournalTresorerieDto update(Long id, JournalTresorerieDto dto) {
+        JournalTresorerie entity = repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("JournalTresorerie introuvable"));
+        validateReferences(dto);
+        mapToEntity(dto, entity);
+        return toDto(repository.save(entity));
+    }
+
+    private void validateReferences(JournalTresorerieDto dto) {
+
+    if (!banqueRepository.existsById(dto.getBanqueId())) {
+        throw new RuntimeException("Banque introuvable");
+    }
+
+    if (!compteBancaireRepository.existsById(dto.getCompteBancaireId())) {
+        throw new RuntimeException("Compte bancaire introuvable");
+    }
+
+    if (!deviseRepository.existsById(dto.getDeviseId())) {
+        throw new RuntimeException("Devise introuvable");
+    }
+}
+
+    // ================= GET BY ID =================
+    @Transactional
+    public JournalTresorerieDto getById(Long id) {
+        return repository.findById(id)
+                .map(this::toDto)
+                .orElseThrow(() -> new RuntimeException("JournalTresorerie introuvable"));
+    }
+
+    // ================= DELETE =================
+    public void delete(Long id) {
+        if (!repository.existsById(id)) {
+            throw new RuntimeException("JournalTresorerie introuvable");
+        }
+        repository.deleteById(id);
+    }
+
+    // ================= FIND ALL AVEC FILTRE =================
+    @Transactional
+    public List<JournalTresorerieDto> findAll(
+            Long exerciceId,
+            Long banqueId,
+            Long compteBancaireId,
+            String numero,
+            OffsetDateTime debut,
+            OffsetDateTime fin) {
+
+        Specification<JournalTresorerie> spec =
+                JournalTresorerieSpecification.filter(
+                        exerciceId, banqueId, compteBancaireId,
+                        numero, debut, fin);
+
+        return repository.findAll(spec)
+                .stream()
+                .map(this::toDto)
+                .toList();
+    }
+
+    // ================= MAPPING =================
+
+    private void mapToEntity(JournalTresorerieDto dto, JournalTresorerie e) {
+
+        e.setNumero(dto.getNumero());
+        e.setIdExercice(dto.getIdExercice());
+        e.setTypebudget(dto.getTypebudget());
+        e.setTypemouvement(dto.getTypemouvement());
+        e.setTaux(dto.getTaux());
+        e.setMontant(dto.getMontant());
+        e.setObjet(dto.getObjet());
+        e.setDate(dto.getDate());
+
+        if (dto.getBanqueId() != null) {
+            e.setBanque(banqueRepository.findById(dto.getBanqueId()).orElse(null));
+        }
+
+        if (dto.getPlanComptableId() != null) {
+            e.setPlanComptable(planComptableRepository.findById(dto.getPlanComptableId()).orElse(null));
+        }
+
+        if (dto.getClasseId() != null) {
+            e.setClasse(classeRepository.findById(dto.getClasseId()).orElse(null));
+        }
+
+        if (dto.getDeviseId() != null) {
+            e.setDevise(deviseRepository.findById(dto.getDeviseId()).orElse(null));
+        }
+
+        if (dto.getCompteBancaireId() != null) {
+            e.setCompteBancaire(compteBancaireRepository.findById(dto.getCompteBancaireId()).orElse(null));
+        }
+
+        if (dto.getLiquidationId() != null) {
+            e.setLiquidation(liquidationRepository.findById(dto.getLiquidationId()).orElse(null));
+        }
+
+        if (dto.getPlanActiviteId() != null) {
+            e.setPlanActivite(planActiviteRepository.findById(dto.getPlanActiviteId()).orElse(null));
+        }
+
+        if (dto.getSourceFinacementId() != null) {
+            e.setSourceFinacement(sourceFinacementRepository.findById(dto.getSourceFinacementId()).orElse(null));
+        }
+    }
+
+    private JournalTresorerieDto toDto(JournalTresorerie e) {
+        return JournalTresorerieDto.builder()
+                .id(e.getId())
+                .numero(e.getNumero())
+                .idExercice(e.getIdExercice())
+                .typebudget(e.getTypebudget())
+                .typemouvement(e.getTypemouvement())
+                .taux(e.getTaux())
+                .montant(e.getMontant())
+                .objet(e.getObjet())
+                .date(e.getDate())
+                .banqueId(e.getBanque() != null ? e.getBanque().getId() : null)
+                .planComptableId(e.getPlanComptable() != null ? e.getPlanComptable().getId() : null)
+                .classeId(e.getClasse() != null ? e.getClasse().getId() : null)
+                .deviseId(e.getDevise() != null ? e.getDevise().getId() : null)
+                .compteBancaireId(e.getCompteBancaire() != null ? e.getCompteBancaire().getId() : null)
+                .liquidationId(e.getLiquidation() != null ? e.getLiquidation().getId() : null)
+                .planActiviteId(e.getPlanActivite() != null ? e.getPlanActivite().getId() : null)
+                .sourceFinacementId(e.getSourceFinacement() != null ? e.getSourceFinacement().getId() : null)
+                .build();
+    }
 }
