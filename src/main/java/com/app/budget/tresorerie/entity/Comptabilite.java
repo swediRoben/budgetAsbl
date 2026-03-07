0109554565de824ff15430a3bfa380d83de11c6e
swediRoben <farajampenda43@gmail.com>
2026-03-07 18:23:46 +0200

    correction comptabilite

--- src/main/java/com/app/budget/tresorerie/entity/Comptabilite.java
+++ src/main/java/com/app/budget/tresorerie/entity/Comptabilite.java
@@ -2,14 +2,15 @@ package com.app.budget.tresorerie.entity;
 
 import java.time.OffsetDateTime;
 import java.util.ArrayList;
-import java.util.List;
-
+import java.util.List; 
 import com.app.budget.constate.TypeJournal;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -51,6 +52,7 @@ public class Comptabilite {
     private TypeJournal type;
     private String reference;
 
-    @OneToMany(mappedBy = "ecriture", cascade = CascadeType.ALL)
-    private List<LigneComptable> lignes = new ArrayList<>();
+@OneToMany(mappedBy = "ecriture", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+@JsonIgnore
+private List<LigneComptable> lignes = new ArrayList<>();
 }
