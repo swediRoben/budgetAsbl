50b81b3498d9be2ca4908dc87c9e97b3086fda2c
swediRoben <farajampenda43@gmail.com>
2026-03-28 17:14:12 +0200

    type operation

--- src/main/java/com/app/budget/tresorerie/entity/OperationComptable.java
+++ src/main/java/com/app/budget/tresorerie/entity/OperationComptable.java
@@ -10,6 +10,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
@@ -23,7 +26,9 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class OperationComptable {
+public class OperationComptable { 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String libelle;
     @Enumerated(EnumType.STRING)
